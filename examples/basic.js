910712860e41cf9aa07fa3cd285aed299732b6a5
Matt Smith <mattonfoot@gmail.com>
2016-01-06 15:44:37 +0000

    wrong arg in example

--- examples/basic.js
+++ examples/basic.js
@@ -7,7 +7,7 @@ $ node ./examples/basic.js <APIKEY>
 
 var logit = require('../lib/');
 
-var apikey = process.argv[0];
+var apikey = process.argv[2];
 
 logit.init( apikey, { logToConsole: true } );
 
